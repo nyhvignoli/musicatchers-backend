45224593310e56dade934a0f60be7f8e9ff9c359
Aline Vignoli <nyhv.contato@gmail.com>
2021-02-23 09:15:59 -0300

    Criei as classes de erro BaseError e MySqlError

--- src/data/UserDatabase.ts
+++ src/data/UserDatabase.ts
@@ -1,15 +1,23 @@
 import { User } from "../business/entities/User";
+import { MySqlError } from "../business/error/MySqlError";
 import { BaseDatabase } from "./BaseDatabase";
 
 export class UserDatabase extends BaseDatabase {
-    public createUser = async (user: User) : Promise<void> => {
-        await BaseDatabase.connection(BaseDatabase.USER_TABLE)
-        .insert({
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            nickname: user.nickname,
-            password: user.password                
-        });
+    public createUser = async (
+        user: User
+    ) : Promise<void> => {
+        try {
+            await BaseDatabase.connection(BaseDatabase.USER_TABLE)
+            .insert({
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                nickname: user.nickname,
+                password: user.password                
+            });
+        } catch (error) {
+            const errorInfo = MySqlError.sqlErrorHandler(error.message);
+            throw new MySqlError(errorInfo.statusCode, errorInfo.message);
+        };
     };
 };
--- src/services/Validator.ts
+++ src/services/Validator.ts
@@ -1,16 +1,18 @@
+import { BaseError } from "../business/error/BaseError";
+
 export class Validator {
 
     public validateEmptyProperties = (input: any) => {
         for (const key in input) {
             if (input[key] !== false && !input[key]) {
-                throw new Error(`The field '${key}' is missing`);
+                throw new BaseError(422, `The field '${key}' is missing`);
             };
         };
     };
 
     public validatePassword = (password: string) => { 
         if (password.length < 6) {
-            throw new Error("Password must be at least 6 characters in length");
+            throw new BaseError(406, "Password must be at least 6 characters in length");
         };
     };    
 };
